550109ff14802e333f442ee10faf36f19db2746f
Geeballer <jibola422@gmail.com>
2025-06-30 21:51:58 +0100

    -- chore: added a resolve to the config

--- craco.config.js
+++ craco.config.js
@@ -1,29 +1,32 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer'),
-        process: require.resolve('process/browser.js'),
-        zlib: require.resolve('browserify-zlib'),
-        url: require.resolve('url'),
-        util: require.resolve('util'),
-        path: require.resolve('path-browserify'),
-        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+        process: require.resolve("process/browser.js"),
+        zlib: require.resolve("browserify-zlib"),
+        url: require.resolve("url"),
+        util: require.resolve("util"),
+        path: require.resolve("path-browserify"),
+        os: require.resolve("os-browserify/browser"),
         fs: false,
         net: false,
         tls: false,
+        resolve: {
+          fallback: { vm: require.resolve("vm-browserify") },
+        },
       };
 
       webpackConfig.plugins = [
         ...webpackConfig.plugins,
         new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser.js',
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser.js",
         }),
       ];
 
